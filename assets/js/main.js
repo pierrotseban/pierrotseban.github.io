ea05a217d53a5b892c4bf78c05860327590af6e9
Pierrot Seban <pierrotseban@gmail.com>
2026-04-17 22:55:07 +0200

    validity html

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-function switchImage(anchor) {
-    const img = anchor.querySelector('img');
-    const images = anchor.dataset.images.split(',');
-    const currentIndex = images.indexOf(img.src.split('/').pop());
-    const nextIndex = (currentIndex + 1) % images.length;
-    img.src = images[nextIndex];
-  }
