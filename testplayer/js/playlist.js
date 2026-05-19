074429db2de1375e31ca36460adfd7afa5e2a303
Pierrot Seban <pierrotseban@gmail.com>
2026-05-19 15:11:31 +0200

    testplayer

---
+++ testplayer/js/playlist.js
@@ -0,0 +1,20 @@
+const playlistData = [
+  {
+    "src": "files/01 - Bach - Invention No. 04.mp3",
+    "title": "Bach - Invention No. 04",
+    "track": "01",
+    "cover": "files/piano.webp"
+  },
+  {
+    "src": "files/02 - Bach - Invention No. 10.mp3",
+    "title": "Bach - Invention No. 10",
+    "track": "02",
+    "cover": "files/piano.webp"
+  },
+  {
+    "src": "files/03 - Bach - Invention No. 13.mp3",
+    "title": "Bach - Invention No. 13",
+    "track": "03",
+    "cover": "files/piano.webp"
+  }
+];
